1f9e95c020702855d22c5c968401267af9a6bbae
Jovioluiz <jovioluiz@hotmail.com>
2020-02-05 19:23:13 -0300

    Create uValidaDcto.pas

---
+++ uValidaDcto.pas
@@ -0,0 +1,17 @@
+unit uValidaDcto;
+
+interface
+
+const
+  {
+    Máscara do CPF
+  }
+  MASCARA_CPF = '!999.999.999-99;0;_';
+  {
+    Máscara do CNPJ
+  }
+  MASCARA_CNPJ = '!99.999.999/9999-99;0;_';
+
+implementation
+
+end.
